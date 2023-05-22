a02f5587685c8ce269d7f5196dcb4515f0884853
= <aspironsv@gmail.com>
2023-05-22 13:14:31 +0500

    change login url

--- src/components/sessions/Login.js
+++ src/components/sessions/Login.js
@@ -11,163 +11,172 @@ import { useDispatch, useSelector } from 'react-redux'
 import { toast, ToastContainer } from 'react-toast'
 
 const Login = () => {
-	const router = useRouter()
-	const [isVerify, setIsVerify] = useState(false)
-	const [isValidate, setIsValidate] = useState({ phone: false, code: false })
-	const [phone, setPhone] = useState('')
-	const [code, setPassword] = useState('')
-	const state = useSelector((state) => state)
-	const lang = state?.lang?.lang
-	const dispatch = useDispatch()
+    const router = useRouter()
+    const [isVerify, setIsVerify] = useState(false)
+    const [isValidate, setIsValidate] = useState({ phone: false, code: false })
+    const [phone, setPhone] = useState('')
+    const [code, setPassword] = useState('')
+    const state = useSelector((state) => state)
+    const lang = state?.lang?.lang
+    const dispatch = useDispatch()
 
-	const loginBtn = async (e) => {
-		e.preventDefault()
-		if (phone.length < 9) {
-			setIsValidate({ ...isValidate, phone: true })
-		} else {
-			try {
-				let res
-				res = await api.login({ phone })
-				if (!res.data.error) {
-					setIsVerify(true)
-					setIsValidate({ ...isValidate, phone: false })
-				} else {
-					alert(res.data.error)
-				}
-			} catch (error) {
-				console.error(error)
-				alert(error.response?.data?.error || 'Error')
-			}
-		}
-	}
+    const validateEmail = (email) => {
+        return email.match(
+            /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+    }
 
-	const verifyBtn = async (e) => {
-		e.preventDefault()
-		if (code === '') {
-			setIsValidate({ ...isValidate, code: true })
-		} else {
-			try {
-				let res
-				res = await api.verify({ phone, code })
-				if (res.data.isOk) {
-					setIsVerify(false)
-					let { accessToken, refreshToken } = res.data
-					localStorage.setItem('accessToken', accessToken)
-					localStorage.setItem('refreshToken', refreshToken)
-					dispatch(setIsActive(!state?.user?.isActive))
-					getUser()(dispatch)
-					setIsValidate({ ...isValidate, code: false })
-					router.push('/profile')
-				} else {
-					toast.error(
-						`${
-							lang === 'uz'
-								? 'Kod xato kiritildi'
-								: 'Код был введен неправильно'
-						}`
-					)
-					setPhone('')
-					setPassword('')
-					setIsVerify(false)
-				}
-			} catch (error) {
-				console.log(error)
-				toast.error(
-					`${
-						lang === 'uz'
-							? 'Kod xato kiritildi'
-							: 'Код был введен неправильно'
-					}`
-				)
-			}
-		}
-	}
-	return (
-		<>
-			<ToastContainer position='top' delay='3000' />
+    const loginBtn = async (e) => {
+        e.preventDefault()
+        if (!validateEmail(phone)) {
+            setIsValidate({ ...isValidate, phone: true })
+        } else {
+            try {
+                let res
+                res = await api.login({ email: phone })
+                console.log(res)
+                if (
+                    res.data.isOk |
+                    (res.data.message ===
+                        'Sizga kod jo`natilgan, emailni tekshirib ko`ring')
+                ) {
+                    setIsVerify(true)
+                    setIsValidate({ ...isValidate, phone: false })
+                } else {
+                    alert(res.data.message)
+                }
+            } catch (error) {
+                console.error(error)
+                alert(error.response?.data?.error || 'Error')
+            }
+        }
+    }
 
-			<StyledSessionCard mx='auto' my='2rem' boxShadow='large'>
-				{isVerify ? (
-					<form className='content'>
-						<H3 textAlign='center' mb='0.5rem'>
-							{lang === 'uz' ? 'Kirish' : 'Вход'}
-						</H3>
+    const verifyBtn = async (e) => {
+        e.preventDefault()
+        if (code === '') {
+            setIsValidate({ ...isValidate, code: true })
+        } else {
+            try {
+                let res
+                res = await api.verify({ email: phone, code })
+                if (res.data.isOk) {
+                    setIsVerify(false)
+                    let { accessToken, refreshToken } = res.data
+                    localStorage.setItem('accessToken', accessToken)
+                    localStorage.setItem('refreshToken', refreshToken)
+                    dispatch(setIsActive(!state?.user?.isActive))
+                    getUser()(dispatch)
+                    setIsValidate({ ...isValidate, code: false })
+                    router.push('/profile')
+                } else {
+                    toast.error(
+                        `${
+                            lang === 'uz'
+                                ? 'Kod xato kiritildi'
+                                : 'Код был введен неправильно'
+                        }`
+                    )
+                    setPhone('')
+                    setPassword('')
+                    setIsVerify(false)
+                }
+            } catch (error) {
+                console.log(error)
+                toast.error(
+                    `${
+                        lang === 'uz'
+                            ? 'Kod xato kiritildi'
+                            : 'Код был введен неправильно'
+                    }`
+                )
+            }
+        }
+    }
+    return (
+        <>
+            <ToastContainer position='top' delay='3000' />
 
-						<TextField
-							mb='0.75rem'
-							name='number'
-							// placeholder="+998 99 999 99 99"
-							label='Maxfiy kodni kiriting'
-							type='number'
-							fullwidth
-							// onBlur={handleBlur}
-							onChange={(e) => setPassword(e.target.value)}
-							style={{
-								borderColor: isValidate.code
-									? 'red'
-									: '#AEB4BE',
-							}}
-							value={code}
-							// errorText={touched.number && errors.number}
-						/>
+            <StyledSessionCard mx='auto' my='2rem' boxShadow='large'>
+                {isVerify ? (
+                    <form className='content'>
+                        <H3 textAlign='center' mb='0.5rem'>
+                            {lang === 'uz' ? 'Kirish' : 'Вход'}
+                        </H3>
 
-						<Button
-							mb='1.65rem'
-							variant='contained'
-							color='primary'
-							type='submit'
-							fullwidth
-							onClick={(e) => verifyBtn(e)}
-						>
-							{lang === 'uz' ? 'Kirish' : 'Вход'}
-						</Button>
-					</form>
-				) : (
-					<form className='content'>
-						<H3 textAlign='center' mb='0.5rem'>
-							{lang === 'uz' ? 'Kirish' : 'Вход'}
-						</H3>
+                        <TextField
+                            mb='0.75rem'
+                            name='number'
+                            // placeholder="+998 99 999 99 99"
+                            label='Maxfiy kodni kiriting'
+                            type='number'
+                            fullwidth
+                            // onBlur={handleBlur}
+                            onChange={(e) => setPassword(e.target.value)}
+                            style={{
+                                borderColor: isValidate.code
+                                    ? 'red'
+                                    : '#AEB4BE',
+                            }}
+                            value={code}
+                            // errorText={touched.number && errors.number}
+                        />
 
-						<FlexBox>
-							<TextField
-								mb='0.75rem'
-								name='number'
-								placeholder='99 999 99 99'
-								label={
-									lang === 'uz'
-										? 'Telefon raqamingizni kiriting'
-										: 'Введите свой номер телефона'
-								}
-								type='number'
-								span={true}
-								spanTitle='+998'
-								onChange={(e) =>
-									setPhone('+998' + e.target.value)
-								}
-								style={{
-									borderColor: isValidate.phone
-										? 'red'
-										: '#AEB4BE',
-								}}
-								fullwidth
-							/>
-						</FlexBox>
+                        <Button
+                            mb='1.65rem'
+                            variant='contained'
+                            color='primary'
+                            type='submit'
+                            fullwidth
+                            onClick={(e) => verifyBtn(e)}
+                        >
+                            {lang === 'uz' ? 'Kirish' : 'Вход'}
+                        </Button>
+                    </form>
+                ) : (
+                    <form className='content'>
+                        <H3 textAlign='center' mb='0.5rem'>
+                            {lang === 'uz' ? 'Kirish' : 'Вход'}
+                        </H3>
 
-						<Button
-							mb='1.65rem'
-							variant='contained'
-							color='primary'
-							type='submit'
-							fullwidth
-							onClick={(e) => loginBtn(e)}
-						>
-							{lang === 'uz' ? 'Kirish' : 'Вход'}
-						</Button>
-					</form>
-				)}
-			</StyledSessionCard>
-		</>
-	)
+                        <FlexBox>
+                            <TextField
+                                mb='0.75rem'
+                                name='email'
+                                placeholder='example@gmail.com'
+                                label={
+                                    lang === 'uz'
+                                        ? 'Emailingizni kiriting'
+                                        : 'Введите свой адрес электронной почты'
+                                }
+                                type='email'
+                                span={false}
+                                // spanTitle='@'
+                                onChange={(e) => setPhone(e.target.value)}
+                                style={{
+                                    borderColor: isValidate.phone
+                                        ? 'red'
+                                        : '#AEB4BE',
+                                }}
+                                fullwidth
+                            />
+                        </FlexBox>
+
+                        <Button
+                            mb='1.65rem'
+                            variant='contained'
+                            color='primary'
+                            type='submit'
+                            fullwidth
+                            onClick={(e) => loginBtn(e)}
+                        >
+                            {lang === 'uz' ? 'Kirish' : 'Вход'}
+                        </Button>
+                    </form>
+                )}
+            </StyledSessionCard>
+        </>
+    )
 }
 
 export default Login
